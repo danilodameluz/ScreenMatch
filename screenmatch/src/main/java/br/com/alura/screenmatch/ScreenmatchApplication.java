65a4bd1e795be491317ae4a266e0da71d94d17d0
danilodameluz <danilo.dameluz@gmail.com>
2026-01-06 17:39:10 -0300

    Alteracao politica CORS. Criacao da classe SerieDTO.

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,20 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//Implementando a interface COMMANDLINERUNNER usaremos a console como "FrontEnd" da aplicação.
-public class ScreenmatchApplication implements CommandLineRunner {
-
-    @Autowired
-    private SerieRepository repositorio;
+public class ScreenmatchApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibirMenu();
 
-	}
 }
